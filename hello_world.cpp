3804b8eec14afdd61e27f34747f9a18d7f1b30fa
davidhalasz <38536685+davidhalasz@users.noreply.github.com>
2018-09-19 18:24:16 +0200

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	cout << "Hello, World!\n"; // Kiírja, hogy "Hello, World!"
+	keep_window_open(); // Amíg nem ütünk le egy karaktert, addig nem lép ki a programból.
+
+	return 0;
+}
